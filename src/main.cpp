2b65fa5b45ac9de34069900f5682e1e3d0d2de8e
Okan Ozbek <devokan@outlook.com>
2026-05-15 20:27:12 +0200

    chore: fix main

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,15 @@
 int main() {
     RadishDB<std::string> db("my_database", 20000); // 5 seconds TTL
 
+    db.Set("key1", "value1");
+
     if (db.Exists("key1")) {
         std::cout << "key1 exists" << std::endl;
     } else {
         std::cout << "key1 does not exist" << std::endl;
     }
 
-    const auto keys = db.Scan();
-    for (const auto& key : keys) {
+    for (const auto keys = db.Scan(); const auto& key : keys) {
         std::cout << key << std::endl;
     }
 
