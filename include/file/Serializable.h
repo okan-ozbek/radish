0a21cd5ece8bc48e8ee472e3fb26cbfaecec06d9
Okan Ozbek <devokan@outlook.com>
2026-05-18 21:22:35 +0200

    feat: added custom serialization logic

---
+++ include/file/Serializable.h
@@ -0,0 +1,19 @@
+//
+// Created by Dorza on 5/18/2026.
+//
+
+#ifndef RADISH_SERIALIZABLE_H
+#define RADISH_SERIALIZABLE_H
+
+#include <iosfwd>
+
+class Serializable {
+public:
+    virtual ~Serializable() = default;
+
+    virtual void Serialize(std::ofstream& out) const = 0;
+    virtual void Deserialize(std::ifstream& in) = 0;
+    virtual void Print() const = 0;
+};
+
+#endif //RADISH_SERIALIZABLE_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,108 +1,65 @@
-#include <iostream>
 
-#include "../include/RadishDB.h"
 
-struct Point2D {
-    int x;
-    int y;
+#include "../include/file/RadishFile.h"
+#include "../include/helpers/Operation.h"
 
-    Point2D() : x(0), y(0) {}
-    explicit Point2D(const int scalar) : x(scalar), y(scalar) {}
-    explicit Point2D(const int x, const int y) : x(x), y(y) {}
+class Test final : public Serializable {
+public:
+    Test() = default;
 
-    int Sum() const {
-        return x + y;
+    explicit Test(std::string name, int age)
+        : name{ std::move(name) }
+        , age{ age }
+    {}
+
+    void Serialize(std::ofstream &out) const override {
+        const uint32_t nameByteSize{ static_cast<uint32_t>(name.size()) };
+        out.write(reinterpret_cast<const char*>(&nameByteSize), sizeof(nameByteSize));
+        out.write(name.data(), nameByteSize);
+
+        out.write(reinterpret_cast<const char*>(&age), sizeof(age));
     }
+
+    void Deserialize(std::ifstream &in) override {
+        uint32_t nameByteSize{};
+        in.read(reinterpret_cast<char*>(&nameByteSize), sizeof(nameByteSize));
+
+        name.resize(nameByteSize);
+        in.read(name.data(), nameByteSize);
+
+        in.read(reinterpret_cast<char*>(&age), sizeof(age));
+    }
+
+    void Print() const override {
+        std::cout << "name: " << name << "\n";
+        std::cout << "age: " << age << "\n";
+    }
+
+private:
+    std::string name{};
+    int age{};
 };
-
-template<typename TValue>
-struct Log {
-    using MsTimestamp = uint64_t;
-    using BinarySize = uint32_t;
-
-    OperationType operationType{};
-    MsTimestamp timestamp{};
-    BinarySize keySize{};
-    std::string key{};
-    BinarySize payloadSize{};
-    TValue payload{};
-
-    Log() = default;
-
-    Log(const OperationType& operationType, const MsTimestamp& timestamp)
-        : operationType(operationType)
-        , timestamp(timestamp)
-    {}
-
-    Log(const OperationType& operationType, const MsTimestamp& timestamp, const std::string& key)
-        : operationType(operationType)
-        , timestamp(timestamp)
-        , keySize(key.size())
-        , key(key)
-    {}
-
-    Log(const OperationType& operationType, const MsTimestamp& timestamp, const std::string& key, const TValue& payload)
-        : operationType(operationType)
-        , timestamp(timestamp)
-        , keySize(key.size())
-        , key(key)
-        , payloadSize(sizeof(TValue))
-        , payload(payload)
-    {}
-};
-
-template<typename TValue>
-void WriteToFile(const Log<TValue>& log) {
-    std::fstream file{};
-
-    file.open("example.radish", std::ios::out | std::ios::binary);
-    if (file.is_open() == false) {
-        throw std::runtime_error("Failed to open file for writing");
-    }
-
-    file.write(reinterpret_cast<const char*>(&log.operationType), sizeof(log.operationType));
-    file.write(reinterpret_cast<const char*>(&log.timestamp), sizeof(log.timestamp));
-    file.write(reinterpret_cast<const char*>(&log.keySize), sizeof(log.keySize));
-    file.write(log.key.c_str(), log.keySize);
-    file.write(reinterpret_cast<const char*>(&log.payloadSize), sizeof(log.payloadSize));
-    file.write(reinterpret_cast<const char*>(&log.payload), sizeof(log.payload));
-    std::cout << "Successfully written to file.\n";
-}
-
-template<typename TValue>
-void ReadFromFile(Log<TValue>* log) {
-    std::fstream file{};
-
-    file.open("example.radish", std::ios::in | std::ios::binary);
-    if (file.is_open() == false) {
-        throw std::runtime_error("Failed to open file for reading");
-    }
-
-    file.read(reinterpret_cast<char*>(&log->operationType), sizeof(log->operationType));
-    file.read(reinterpret_cast<char*>(&log->timestamp), sizeof(log->timestamp));
-    file.read(reinterpret_cast<char*>(&log->keySize), sizeof(log->keySize));
-    file.read(reinterpret_cast<char*>(&log->key), log->keySize);
-    file.read(reinterpret_cast<char*>(&log->payloadSize), sizeof(log->payloadSize));
-    file.read(reinterpret_cast<char*>(&log->payload), sizeof(log->payload));
-    std::cout << "Successfully read from file.\n";
-}
 
 int main() {
-    const Point2D payload{ 10, 20 };
-    const Log sent{ SET, 1234567890, "point:val:1", payload };
-    Log<Point2D> received{};
+    RadishFile<Test> rf{ "test.radish", "./" };
+    //
+    // const RadishRow<int> row{ DELETE, 123, "key1", 42 };
+    // const RadishRow<int> row2{ DELETE, 123, "key2", 42 };
+    // const RadishRow<int> row3{ DELETE, 123, "key3", 42 };
 
-    WriteToFile(sent);
-    ReadFromFile(&received);
+    const RadishRow<Test> tr1{ SET, 123, "key1", Test{ "Okan", 30 } };
+    const RadishRow<Test> tr2{ SET, 321, "key2", Test{ "Dorza", 25 } };
 
-    std::cout << "Operation Type: " << TryGetNameByOperationType(received.operationType).value_or("Unknown") << "\n";
-    std::cout << "Timestamp: " << received.timestamp << "\n";
-    std::cout << "Key: " << received.key << "\n";
-    std::cout << "Payload: (" << received.payload.x << ", " << received.payload.y << ")\n";
+    rf.Append(tr1);
+    rf.Append(tr2);
 
-    // RadishDB<std::string> db("my_database", 20000); // 5 seconds TTL
+    int i{};
+    for (const std::vector<RadishRow<Test>> values = rf.ReadAll(); auto& value : values) {
+        std::cout << i << " -------------------\n";
 
-    std::cout << sizeof(long long) << " vs. " << sizeof(uint64_t) << std::endl;
+        value.Print();
+        ++i;
+    }
 
     return 0;
 }
