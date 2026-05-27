5b985fcf5093d073a6c1b0d233df82fbf3fde91b
Okan Ozbek <devokan@outlook.com>
2026-05-28 00:16:25 +0200

    feat: olc tcp training

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,60 @@
 #include <iostream>
 
-#include "../include/RadishDB.h"
+#ifndef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif // _WIN32
+
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+// #include "../include/RadishDB.h"
+
+std::vector<char> buffer(20 * 1024);
+
+void GetAllBytes(asio::ip::tcp::socket& socket) {
+    socket.async_read_some(
+        asio::buffer(buffer.data(), buffer.size()),
+        [&socket](const std::error_code errorCode, const std::size_t length) {
+            if (errorCode) return;
+
+            std::cout << "\n\nRead: " << length << " bytes\n\n";
+            for (std::size_t i{}; i < length; ++i) {
+                std::cout << buffer[i];
+            }
+
+            GetAllBytes(socket);
+        }
+    );
+}
 
 int main() {
-    std::vector example = { std::byte{ 0x01 }, std::byte{ 0x02 }, std::byte{ 0x03 }, std::byte{ 0x04 } };
+    asio::error_code errorCode;
+    asio::io_context context;
 
-    RadishDB<std::vector<std::byte>> db("mydb");
 
-    db.Create("key1", example);
 
-    if (const auto val = db.Get("key1"); val.value() == example) {
-        std::cout << "Value retrieved successfully!" << std::endl;
-    } else {
-        std::cout << "Value not retrieved successfully!" << std::endl;
+    asio::ip::tcp::endpoint endpoint(asio::ip::make_address("51.38.81.49", errorCode), 80);
+    asio::ip::tcp::socket socket(context);
+
+    socket.connect(endpoint);
+
+    if (errorCode) {
+        std::cerr << "Failed to connect to server: " << errorCode.message() << std::endl;
+        return 1;
     }
 
-    return std::cin.get();
+    if (socket.is_open()) {
+        GetAllBytes(socket);
+
+        std::string request =
+            "GET /index.html HTTP/1.1\r\n"
+            "Host: example.com\r\n"
+            "Connection: close\r\n\r\n";
+
+        socket.write_some(asio::buffer(request.data(), request.size()), errorCode);
+    }
+
+    return 0;
 }
